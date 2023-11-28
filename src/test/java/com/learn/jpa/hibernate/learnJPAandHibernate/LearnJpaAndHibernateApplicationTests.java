1d20eddc43b36af09f0ba8b58c54fd84789cbfa4
Muhammad Abdullah 05 <muhammad.abdullah05@confiz.local>
2023-11-28 16:20:02 +0500

    initial commit

---
+++ src/test/java/com/learn/jpa/hibernate/learnJPAandHibernate/LearnJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.jpa.hibernate.learnJPAandHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
